a304cbe917997926299bb528629d7461ea67a52e
Young Soo Koh <kohys92@gmail.com>
2021-08-25 21:39:23 +0900

    Add: models created

--- oranges/models.py
+++ oranges/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Orange(models.Model):
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=1000)
+    phone_number = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'oranges'
+
